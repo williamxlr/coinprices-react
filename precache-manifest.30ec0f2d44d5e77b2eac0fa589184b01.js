cc924053baff73e54ba390001b45cd1a4bd16851
williamxlr <wara65@hotmail.com>
2020-05-13 17:18:11 -0500

    Updates

---
+++ precache-manifest.30ec0f2d44d5e77b2eac0fa589184b01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2932711914790ea5bd0374a28ac7014a",
+    "url": "/coinprices-react/index.html"
+  },
+  {
+    "revision": "ccd113645cedfffb20c3",
+    "url": "/coinprices-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "04713ac5c2bbaeb07a81",
+    "url": "/coinprices-react/static/css/main.b062d5e2.chunk.css"
+  },
+  {
+    "revision": "ccd113645cedfffb20c3",
+    "url": "/coinprices-react/static/js/2.7a551fd7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coinprices-react/static/js/2.7a551fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04713ac5c2bbaeb07a81",
+    "url": "/coinprices-react/static/js/main.9cbb233c.chunk.js"
+  },
+  {
+    "revision": "bde3a769bb80769ac7ac",
+    "url": "/coinprices-react/static/js/runtime-main.3b8a7a2a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coin-prices-react/precache-manifest.19de7b92b2bdcaa47f19448305e28bc3.js"
+  "/coinprices-react/precache-manifest.30ec0f2d44d5e77b2eac0fa589184b01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coin-prices-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coinprices-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
